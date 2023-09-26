bed0526f8e05c429918b4bb28691658525b03896
Mikoshiba-Kyu <mikoshibakyu@gmail.com>
2023-09-26 14:41:46 +0900

    end work

--- src/components/UI/BlankContents.tsx
+++ src/components/UI/BlankContents.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import SmsOutlinedIcon from '@mui/icons-material/SmsOutlined'
 
 interface Props {
